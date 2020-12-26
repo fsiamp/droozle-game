16d65ddff84f5d237523fd46858a349935a861e2
Fanis Siampos <fsiamp@gmail.com>
2020-12-26 16:11:37 +0200

    Set decimal values in input field

--- javascript/loadXML.js
+++ javascript/loadXML.js
@@ -31,10 +31,10 @@ function reply_click(clicked_id,questions) {
 		success: function(xml){
 			   if ($(xml).find("correct").text().includes(clicked_id)) {
   
-				 document.getElementById("score").value = parseInt(document.getElementById("score").value) + 2000;
+				 document.getElementById("score").value = parseInt(document.getElementById("score").value) + 2000.000000;
 			   }
 			   else {
-				 document.getElementById("score").value = parseInt(document.getElementById("score").value) - 1000;
+				 document.getElementById("score").value = parseInt(document.getElementById("score").value) - 1000.000000;
 				 if (document.getElementById("score").value < 0) {
 				   document.getElementById("score").value = 0;
 				 }
