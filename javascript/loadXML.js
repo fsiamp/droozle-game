92218ed5eb8fd627871bd325ebc34dea06c523d7
Fanis Siampos <fsiamp@gmail.com>
2021-04-03 01:14:36 +0300

    Added five new questions

--- javascript/loadXML.js
+++ javascript/loadXML.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 		dataType: "xml",
 		success: function(xml){
 		    var questions = [];
-			var random = Math.floor(Math.random() * 186);
+			var random = Math.floor(Math.random() * 191);
 			questions.push(random);
 			var sText = $(xml).find('text').eq(random).text();
 			var sOptionA = $(xml).find('optionA').eq(random).text();
@@ -43,11 +43,11 @@ function reply_click(clicked_id,questions) {
 			var flag = 0;
 			var updatedArray = questions.split(',');
 			//console.log(updatedArray);
-			if (updatedArray.length == 186) {
+			if (updatedArray.length == 191) {
 			    updatedArray = [];
 			}
 			function getRand() {
-				var rand = Math.floor(Math.random() * 186);
+				var rand = Math.floor(Math.random() * 191);
 				//console.log(rand);
 				if ($.inArray(rand.toString(), updatedArray) === -1) {
 					return rand;
