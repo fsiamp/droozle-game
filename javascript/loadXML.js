b2a47aa5383b62e92d06be4aab656c2c975163df
Fanis Siampos <fsiamp@Faniss-iMac.local>
2021-09-12 11:56:49 +0300

    Modified javascript file

--- javascript/loadXML.js
+++ javascript/loadXML.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 		dataType: "xml",
 		success: function(xml){
 		    var questions = [];
-			var random = Math.floor(Math.random() * 255);
+			var random = Math.floor(Math.random() * 256);
 			questions.push(random);
 			var sText = $(xml).find('text').eq(random).text();
 			var sOptionA = $(xml).find('optionA').eq(random).text();
@@ -43,11 +43,11 @@ function reply_click(clicked_id,questions) {
 			var flag = 0;
 			var updatedArray = questions.split(',');
 			//console.log(updatedArray);
-			if (updatedArray.length == 255) {
+			if (updatedArray.length == 256) {
 			    updatedArray = [];
 			}
 			function getRand() {
-				var rand = Math.floor(Math.random() * 255);
+				var rand = Math.floor(Math.random() * 256);
 				//console.log(rand);
 				if ($.inArray(rand.toString(), updatedArray) === -1) {
 					return rand;
